497d06cb1537e112db8a2ae0f8610d3ee1fb2e66
RubenSomsen <rsomsen@gmail.com>
2022-07-11 20:13:59 +0200

    Fixed linux path

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Contains puzzles from 2019, 2020 (complete), 2021
 // Just change a00_00 to year and puzzle, e.g. a19_01
 
-#[path = "puzzles/a21_19.rs"] mod m2119;
+#[path = "./puzzles/a21_18.rs"] mod m2118;
 
 fn main() {
 
@@ -9,10 +9,10 @@ fn main() {
     let now = Instant::now();
 
     let path = std::env::current_dir().unwrap();
-    let full_path = path.to_str().unwrap().to_owned() + "\\src\\puzzles\\input\\";
+    let full_path = path.to_str().unwrap().to_owned() + "/src/puzzles/input/";
     let extension = ".txt";
     
-    m2119::run(&(full_path.to_owned() + "a21_19" + extension));
+    m2118::run(&(full_path.to_owned() + "a21_18" + extension));
 
     let elapsed = now.elapsed();
     println!("PERFORMANCE TOTAL: {:?}", elapsed);
